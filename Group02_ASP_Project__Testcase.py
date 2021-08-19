4872e587910f91c2b52a713afbedd6b1a5c54b20
YeZhiXiu <87739791+YeZhiXiu@users.noreply.github.com>
2021-08-20 00:12:42 +0800

    Update Group02_ASP_Project__Testcase.py

--- Group02_ASP_Project__Testcase.py
+++ Group02_ASP_Project__Testcase.py
@@ -1,7 +1,12 @@
-class EvaluateMarks:
+import unittest
+import Group02_ASP_Project as prog
 
-    def total(n1):
-        return sum(n1)
+class Evaluate(unittest.TestCase):
+    def test_total(self):
+        self.assertEqual(prog.total, 28664611)
 
-    def mean(a, b):
-        return a / b
+    def test_mean(self):
+        self.assertEqual(prog.mean, 9554870.34)
+
+if __name__ == '__main__':
+    unittest.main()
